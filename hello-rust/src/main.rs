047133bacfce230ed9e318dba0c2b3690358c854
ziaddorbuk <49367279+ziaddorbuk@users.noreply.github.com>
2025-01-21 00:15:45 +0200

    feat(hello-rust): add new rand feat

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,16 @@
+use rand::Rng;
+
 fn main() {
     println!("change the world!");
+    generate random number between 0 and 20
+    let mut rng = rand::thread_rng();
+    let random_number: u32 = rng.gen_range(0..=20);
+    println!("Random number: {}", random_number);
+
+    if random_number > 10 {
+        println!("The number is greater than 10: {}", random_number);
+    }
+    else {
+        println!("The number is less than or equal to 10: {}", random_number);
+    }
 }
